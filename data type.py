cb88712784398dd5f2fc42e8d4bf4e23dffb4e65
shreyashbabar07 <shreyashbabar91@gmail.com>
2026-01-08 14:59:17 +0530

    learn about data types

---
+++ data type.py
@@ -0,0 +1,66 @@
+#Program 1: Integer data type
+# Integer data type: 
+a = 100
+
+print("Value of a:", a)
+print("Data Type:", type(a))
+
+
+#Program 2: Float data type 
+# Float data type
+b = 25.75
+
+print("Value of b:", b)
+print("Data Type:", type(b))
+
+
+#Program 3: String 
+# String data type
+name = "Shreyash"
+
+print("Name:", name)
+print("Data Type:", type(name))
+
+
+#Program 4: Boolean Variable
+# Boolean data type
+is_python_fun = True
+
+print("Python is fun:", is_python_fun)
+print("Data Type:", type(is_python_fun))
+
+
+#Program 5: List
+# List data type
+numbers = [10, 20, 30, 40]
+
+print("Numbers:", numbers)
+print("Data Type:", type(numbers))
+
+
+#Program 6: Tuple
+# Tuple data type
+colors = ("Red", "Green", "Blue")
+
+print("Colors:", colors)
+print("Data Type:", type(colors))
+
+
+#Program 7: Set
+## Set data type
+fruits = {"Apple", "Banana", "Mango", "Apple"}
+
+print("Fruits:", fruits)   # Apple will appear only once
+print("Data Type:", type(fruits))
+
+#Program 8: Dictionary
+# Dictionary data type
+student = {
+    "name": "Shreyash",
+    "age": 22,
+    "course": "AIML"
+}
+
+print("Student Details:", student)
+print("Data Type:", type(student))
+

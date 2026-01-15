df0d00caf02236fc83e72003b3a14ab5256af522
shreyashbabar07 <shreyashbabar91@gmail.com>
2026-01-15 14:45:18 +0530

    learn about Operators

---
+++ operators.py
@@ -0,0 +1,60 @@
+# Arethmatic Operators
+a = 7 
+b = 4
+c = a + b 
+print(c)
+
+# Assignment Operators
+a = 4
+b = 2
+c = a - b
+print(c)
+
+# Anathor method 
+a = 4-2 # Assingn 4-2 in a
+print(a)
+
+# Incremet and Decrement
+b = 6
+b += 3 # Increment the value by 3 and then assign it to b
+b -= 3 # Decrement the value by 3 and then assign it to b
+print(b)
+
+# Comparison operator
+
+d = 5<4  # 5 is less than 4
+print(d)
+
+d = 5>4  # 5 is greater than 4
+print(d)
+
+d = 5<=4 # 5 is less than is equal to 4
+print (d)
+
+d = 5>=4  # 5 is greater than is equal to 4
+print (d)
+
+d = 5!=5 # 5 is not  equal to 5
+print(d)
+
+d = 5==5 #  Is 5 equal to 5 
+print(d)
+
+# Logicaal Operators
+e = True or False
+print(e)
+
+# Truth Table of 'or'
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth Table of 'and'
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+
+print(not(True)) # not operator

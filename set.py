ed4087cbfe8d73a338b26c736970bf8f57252733
Shreyashbabar@07 <Shreyashbabaar91@gmail.com>
2026-01-16 15:03:53 +0530

    Learn about Sets in python

---
+++ set.py
@@ -0,0 +1,14 @@
+# Creating two set
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+# Finding Union of Sets
+union_set = A.union(B)
+
+# Finding Intersection of Sets
+intersection_set = A.intersection(B)
+
+# Printing results
+print("Union:", union_set)
+print("Intersection:", intersection_set)

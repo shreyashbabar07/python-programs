35e7759738bcb06c5dee9c63075b5c0bbf71251c
Shreyashbabar@07 <Shreyashbabaar91@gmail.com>
2026-01-16 13:55:39 +0530

    Learn about Array

---
+++ array2.py
@@ -0,0 +1,11 @@
+import array as arr
+
+# Creating an array
+numbers = arr.array('i',[10, 20, 30, 40])
+
+# inserting element at index2
+numbers.insert(2, 25)
+
+# Displaying updated array
+print ("Array after insertion :", numbers)
+

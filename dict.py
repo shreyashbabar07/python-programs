4e6a487b97c504271dffa225cf2d16d27879f632
Shreyashbabar@07 <Shreyashbabaar91@gmail.com>
2026-01-16 14:35:52 +0530

    Learn about Creating a dictionary for stident details

---
+++ dict.py
@@ -0,0 +1,12 @@
+# Creating a dictionary for stident details
+student = {
+    "Name" : "Shreyash",
+    "Math" : 86,
+    "Science" : 92,
+    "AI" : 90
+}
+    
+# Printing all key-value pairs
+print("Student Details:")
+for key, value in student.items():
+    print(key, ":", value)

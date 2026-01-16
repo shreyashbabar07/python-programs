3093df3abc42b074253ba200444f3fbeb62b7205
Shreyashbabar@07 <Shreyashbabaar91@gmail.com>
2026-01-16 14:02:25 +0530

    Learn about array

---
+++ list1.py
@@ -0,0 +1,12 @@
+# Creating a list of numbers 
+nums = [12, 45, 2, 18, 900, 32]
+
+# Finding a largest number in the list 
+largest = max(nums)
+
+# Finding a Smallest number in the list
+smallest = min(nums)
+
+# printing the results
+print("Largest number:", largest)
+print("smalleat number:", smallest)

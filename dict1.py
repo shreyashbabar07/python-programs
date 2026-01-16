76023c384d8ed6567b21238824de774869c9b5b0
Shreyashbabar@07 <Shreyashbabaar91@gmail.com>
2026-01-16 14:41:48 +0530

    leatn about Creating a dictionary of subjects and marks

---
+++ dict1.py
@@ -0,0 +1,12 @@
+# Creating a dictionary of subjects and marks
+marks = {
+    "Math" : 80,
+    "Physics" : 90,
+    "Chemistry" : 85
+}
+
+# Calculating total marks
+total = sum(marks.values())
+
+# Displaying the total
+print("Total marks:", total)

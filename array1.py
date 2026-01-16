01eb139caade24c6cdc9717e2dc00d4bf8473a97
Shreyashbabar@07 <Shreyashbabaar91@gmail.com>
2026-01-16 13:51:00 +0530

    learn about Array

---
+++ array1.py
@@ -0,0 +1,8 @@
+import array as arr
+
+# Creating an array
+numbers = arr.array('i',[10, 15, 20, 25, 30])
+
+# Accessing element using index
+print("Firtst element:", numbers[3])
+print("Last element:", numbers[-1])

01ca33c9952cc4c11fc1f2038c6a05eee37e88e6
Shreyashbabar@07 <shreyashbabar91@gmail.com>
2026-01-22 20:35:27 +0530

    Learn about Multiple Inheritance

---
+++ 03_multiple_inheritance.py
@@ -0,0 +1,25 @@
+'''Multiple Inheritance : Multiple Inheritance occurs when the child class inherits from more than parent class'''
+
+class Employee:
+    name = "Default name"
+    company = "ITC"
+    def show(self):
+        print(f"The name of the Employee is {self.name} and the company is {self.company}")
+
+class Coder:
+    language= "Python"
+    def printLanguages(self):
+        print(f"Out of all the languages here is your language: {self.language}")
+    
+class Programmer(Employee, Coder):
+    company = "ITC Infotech"
+    def showLanguage(self):
+        print(f"The name is {self.company} and he is good with {self.language} language")
+         
+a = Employee()
+b = Programmer()
+
+b.show()
+b.printLanguages()
+b.showLanguage()
+
